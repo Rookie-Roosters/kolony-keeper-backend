ec1adbb60ba00fa65e1f660c0814a96c0f253bed
Leonel Fernández <leonel-leonel-1@hotmail.com>
2023-04-11 22:50:15 -0600

    Deploy test

--- src/modules/devices/dto/create-device.dto.ts
+++ src/modules/devices/dto/create-device.dto.ts
@@ -1,1 +1,30 @@
-export class CreateDeviceDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateDeviceDto {
+  @ApiProperty()
+  user: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty({
+    type: Number,
+    enum: [
+      'IOS',
+      'ANDROID',
+      'WEB',
+      'WINDOWS',
+      'MACOS',
+      'LINUX',
+      'EMAIL',
+      'SMS',
+    ],
+  })
+  platform: number;
+
+  @ApiProperty({
+    type: String,
+    maxLength: 256,
+  })
+  token: string;
+}
--- src/modules/devices/entities/device.entity.ts
+++ src/modules/devices/entities/device.entity.ts
@@ -5,6 +5,17 @@ import { User } from "src/modules/users/entities/user.entity";
 
 export type DeviceDocument = Device & Document;
 
+export enum DevicePlatform {
+    IOS,
+    ANDROID,
+    WEB,
+    WINDOWS,
+    MACOS,
+    LINUX,
+    EMAIL,
+    SMS
+}
+
 @Schema()
 export class Device {
     @ApiProperty()
@@ -16,6 +27,25 @@ export class Device {
         ref: 'User'
     })
     user: User;
+
+    @ApiProperty()
+    @Prop({
+        type: String,
+        maxlength: 256,
+    })
+    name: string;
+
+    @ApiProperty({})
+    @Prop({
+        type: Number
+    })
+    platform: DevicePlatform;
+
+    @ApiProperty({
+        type: String,
+        maxLength: 256
+    })
+    token: string;
 }
 
 export const DeviceSchema = SchemaFactory.createForClass(Device);
